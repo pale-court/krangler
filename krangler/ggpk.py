30fac15b76ec494ae1117f346ec09fe7091d3c45
Lars Viklund <zao@zao.se>
2023-12-15 23:27:49 +0100

    feat: add GGPK decompose script

--- krangler/ggpk.py
+++ krangler/ggpk.py
@@ -5,79 +5,101 @@ from dataclasses import dataclass
 import struct
 from typing import List
 
-def _chunk_tag(s: str):
-    return struct.unpack('<I', struct.pack('<BBBB', ord(s[0]), ord(s[1]), ord(s[2]), ord(s[3])))[0]
+_GGPK_TAG = b'GGPK'
+_FILE_TAG = b'FILE'
+_PDIR_TAG = b'PDIR'
+_FREE_TAG = b'FREE'
 
-_GGPK_TAG = _chunk_tag('GGPK')
-_FILE_TAG = _chunk_tag('FILE')
-_PDIR_TAG = _chunk_tag('PDIR')
-_FREE_TAG = _chunk_tag('FREE')
 
 @dataclass
-class PackDir:
+class PackRecord:
+    rec_len: int
+    tag: str
+
+
+@dataclass
+class PackDir(PackRecord):
     name: str
     sha256: bytes
     children: List[int]
 
+
 @dataclass
-class PackFile:
+class PackFile(PackRecord):
     name: str
     sha256: bytes
     data_offset: int
     data_size: int
 
+
+@dataclass
+class PackFree(PackRecord):
+    next_free: int
+    data_offset: int
+    data_size: int
+
+
 class PackSource(source.Source):
     def __init__(self, fh):
         self.fh = fh
-        self.dirs = {}
-        self.files = {}
-        self.parents = {}
+        self.dirs: dict[int, PackDir] = {}
+        self.files: dict[int, PackFile] = {}
+        self.frees: dict[int, PackFree] = {}
+        self.parents: dict[int, int] = {}
 
         pack_offset = 0
         fh.seek(0, io.SEEK_END)
         pack_size = fh.tell()
         fh.seek(0, io.SEEK_SET)
 
-        rec_len, tag, version, child0, child1 = struct.unpack('<IIIQQ', fh.read(28))
+        self.ggpk = fh.read(28)
+        rec_len, tag, version, child0, child1 = struct.unpack('<I4sIQQ', self.ggpk)
         if tag != _GGPK_TAG or version not in (2, 3):
             raise RuntimeError("Invalid GGPK chunk")
         pack_offset = fh.tell()
         while pack_offset < pack_size:
-            rec_len, tag = struct.unpack('<II', fh.read(8))
+            rec_len, tag = struct.unpack('<I4s', fh.read(8))
             next_offset = pack_offset + rec_len
             if tag == _PDIR_TAG:
                 if pack_offset in (child0, child1):
                     self.root_offset = pack_offset
                 name_len, child_count = struct.unpack('<II', fh.read(8))
                 sha256 = fh.read(32)
-                name = fh.read(name_len*2)[:-2].decode('UTF-16LE')
+                name = fh.read(name_len * 2)[:-2].decode('UTF-16LE')
                 children = []
                 for _ in range(child_count):
                     name_hash, child_offset = struct.unpack('<IQ', fh.read(12))
                     children.append(child_offset)
                     self.parents[child_offset] = pack_offset
-                self.dirs[pack_offset] = PackDir(name=name, sha256=sha256, children=children)
+                self.dirs[pack_offset] = PackDir(rec_len=rec_len, tag=tag, name=name, sha256=sha256, children=children)
             elif tag == _FILE_TAG:
                 name_len, = struct.unpack('<I', fh.read(4))
                 sha256 = fh.read(32)
-                name = fh.read(name_len*2)[:-2].decode('UTF-16LE')
+                name = fh.read(name_len * 2)[:-2].decode('UTF-16LE')
                 data_offset = fh.tell()
                 data_size = rec_len - (data_offset - pack_offset)
-                self.files[pack_offset] = PackFile(name=name, sha256=sha256, data_offset=data_offset, data_size=data_size)
+                self.files[pack_offset] = PackFile(rec_len=rec_len, tag=tag, name=name, sha256=sha256,
+                                                   data_offset=data_offset,
+                                                   data_size=data_size)
             elif tag == _FREE_TAG:
-                pass
+                next_free, = struct.unpack('<Q', fh.read(8))
+                data_offset = fh.tell()
+                data_size = rec_len - (data_offset - pack_offset)
+                self.frees[pack_offset] = PackFree(rec_len=rec_len, tag=tag, next_free=next_free,
+                                                   data_offset=data_offset,
+                                                   data_size=data_size)
             else:
-                print(struct.pack('<I', tag))
+                print(struct.pack('<4s', tag))
                 raise RuntimeError(f"Invalid chunk tag at offset {pack_offset}")
             fh.seek(next_offset, io.SEEK_SET)
             pack_offset = next_offset
 
     def __enter__(self):
         return self
-    
+
     def __exit__(self, *args):
         self.fh.close()
-    
+
     def file_path(self, offset):
         file = self.files[offset]
         segments = [file.name]
@@ -90,11 +112,16 @@ class PackSource(source.Source):
             segments.append(dir.name)
             offset = dir_offset
         return "/".join(reversed(segments))
-    
+
     def file_data(self, offset) -> bytes:
         file = self.files[offset]
         self.fh.seek(file.data_offset)
         return self.fh.read(file.data_size)
+
+    def read_at(self, offset: int, size: int) -> bytes:
+        self.fh.seek(offset)
+        return self.fh.read(size)
+
 
 def open_pack_source(fh: BytesIO) -> source.Source:
     return PackSource(fh)
---
+++ krangler/scripts/ggpk_decompose.py
@@ -0,0 +1,126 @@
+import struct
+import sys
+from contextlib import contextmanager
+from dataclasses import dataclass
+from io import BytesIO
+from pathlib import Path
+from krangler.ggpk import PackSource
+from atomicwrites import atomic_write
+from zstandard import ZstdCompressor
+
+
+@dataclass
+class DecomposedPack:
+    skeleton: bytes
+    free_list: bytes
+
+
+class DataPile:
+    def __init__(self, root: Path):
+        self.root = root
+        if not root.exists():
+            root.mkdir(parents=True, exist_ok=True)
+        for part in "0123456789abcdef":
+            (root / part).mkdir(exist_ok=True)
+
+    def _item_path(self, sha256: bytes) -> Path:
+        hash_hex = sha256.hex()
+        return self.root / hash_hex[0] / f'{hash_hex}.bin'
+
+    def has_one(self, sha256: bytes) -> bool:
+        return self._item_path(sha256).exists()
+
+    def write_one(self, sha256: bytes, payload: bytes):
+        p = self._item_path(sha256)
+        try:
+            with atomic_write(p, mode='wb', overwrite=False) as writer:
+                writer.write(payload)
+        except (FileExistsError, PermissionError) as e:
+            pass
+
+
+def decompose_ggpk(pack: PackSource, pile: DataPile) -> DecomposedPack:
+    # Store data payload on pile
+    for file_offset in pack.files:
+        file_chunk = pack.files[file_offset]
+        file_path = pack.file_path(file_offset)
+        if file_path is not None:
+            if not pile.has_one(file_chunk.sha256):
+                file_data = pack.file_data(file_offset)
+                pile.write_one(file_chunk.sha256, file_data)
+
+    # Generate dense skeleton file
+    skeleton = BytesIO()
+
+    # Write skeleton header
+    rec_len = 4 + 4 + 4 + len(pack.ggpk)
+    skeleton.write(struct.pack(f'<I4sI{len(pack.ggpk)}s', rec_len, b'GGSK', 1, pack.ggpk))
+    write_offset = rec_len
+
+    # Write offset list
+    dir_count = len(pack.dirs)
+    file_count = len(pack.files)
+    rec_len = 4 + 4 + 4 + 4 + dir_count * (8 + 8) + file_count * (8 + 8)
+    write_offset += rec_len
+    data_start = write_offset
+    skeleton.write(struct.pack('<I4sII', rec_len, b'OFFS', dir_count, file_count))
+    for dir_offset, chunk in pack.dirs.items():
+        skeleton.write(struct.pack('<QQ', write_offset, dir_offset))
+        write_offset += chunk.rec_len
+    for file_offset, chunk in pack.files.items():
+        skeleton.write(struct.pack('<QQ', write_offset, file_offset))
+        write_offset += chunk.rec_len
+
+    # Write entries
+    assert data_start == skeleton.tell()
+    for offset, chunk in pack.dirs.items():
+        skeleton.write(pack.read_at(offset, chunk.rec_len))
+    for offset, chunk in pack.files.items():
+        skeleton.write(pack.read_at(offset, chunk.rec_len - chunk.data_size))
+
+    # Generate free list file
+    zctx = ZstdCompressor()
+    free_list = BytesIO()
+    with zctx.stream_writer(free_list, closefd=False) as writer:
+        free_index: dict[int, int] = {}
+
+        header_size = 4 + 4 + 4 + len(pack.frees) * (8 + 8)
+        print(f'{len(pack.frees)=}, {header_size=}')
+        write_offset = header_size
+        data_start = write_offset
+        for free_offset, chunk in pack.frees.items():
+            free_index[write_offset] = free_offset
+            write_offset += chunk.rec_len
+        writer.write(struct.pack('<I4sII', header_size, b'GGFR', 1, len(free_index)))
+        for here, there in free_index.items():
+            writer.write(struct.pack('<QQ', here, there))
+
+        for free_offset in pack.frees:
+            free_chunk = pack.frees[free_offset]
+            writer.write(pack.read_at(free_offset, free_chunk.rec_len))
+
+    return DecomposedPack(skeleton=skeleton.getvalue(), free_list=free_list.getvalue())
+
+
+def main():
+    source_path = Path(sys.argv[1])
+    pile_path = Path(sys.argv[2])
+    out_path = Path(sys.argv[3])
+    gid = sys.argv[4]
+    with source_path.open(mode='rb') as pack_fh:
+        pack = PackSource(pack_fh)
+        pile = DataPile(pile_path)
+        dp = decompose_ggpk(pack, pile)
+        print(f"{len(dp.skeleton)=}, {len(dp.free_list)=}")
+        skel_path = out_path / f'Content-{gid}.ggpk-skeleton'
+        free_path = out_path / f'Content-{gid}.ggpk-free'
+        with (
+            atomic_write(skel_path, mode='wb') as sfh,
+            atomic_write(free_path, mode='wb') as ffh,
+        ):
+            sfh.write(dp.skeleton)
+            ffh.write(dp.free_list)
+
+
+if __name__ == "__main__":
+    main()
